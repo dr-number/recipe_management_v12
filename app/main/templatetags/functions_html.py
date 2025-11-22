3319a5503e5813db9210a50c6fc50d24321fb030
dr.number <dr.number@yandex.ru>
2025-11-22 17:30:40 +0700

    correct host

--- app/main/templatetags/functions_html.py
+++ app/main/templatetags/functions_html.py
@@ -1,9 +1,8 @@
 from django import template
-from main.aes import aes_decrypt
-from app.settings import IMG_PASSWORD
+from app import settings
 
 register = template.Library()
 
 @register.filter
-def decrypt(text):
-    return aes_decrypt(text=text, password=IMG_PASSWORD)
+def get_const(text):
+    return getattr(settings, text, '')
