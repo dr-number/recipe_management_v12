731d29b4eee0ddfadce0faf5b4b6b9c65f842a6d
Lar97 <dr.number@yandex.ru>
2025-11-23 12:02:18 +0700

    off celery for windows

--- app/app/celery.py
+++ app/app/celery.py
@@ -1,35 +1,24 @@
 import os
-
-from celery import Celery
-from celery.schedules import crontab
-
-from app.settings import DEBUG
+from app.settings import IS_WNDOWS
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
-app = Celery('app')
+if not IS_WNDOWS:
+    from celery import Celery
 
-app.conf.beat_schedule = {
-    'clear_finam_catalog_tools': {
-        'task': 'clear_finam_catalog_tools',
-        'schedule': crontab(hour=7, minute=50, day_of_week='6'),
-    },
-    'task_add_log_portfolio': {
-        'task': 'task_add_log_portfolio',
-        'schedule': crontab(hour=0, minute=0, day_of_week='1-5'),
-    },
-}
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
+    app = Celery('app')
 
-app.conf.update(
-    worker_hijack_root_logger=False,
-)
+    # Using a string here means the worker doesn't have to serialize
+    # the configuration object to child processes.
+    # - namespace='CELERY' means all celery-related configuration keys
+    #   should have a `CELERY_` prefix.
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
+    app.conf.update(
+        worker_hijack_root_logger=False,
+    )
 
-# Load task modules from all registered Django apps.
-app.autodiscover_tasks()
+    app.config_from_object('django.conf:settings', namespace='CELERY')
+
+    # Load task modules from all registered Django apps.
+    app.autodiscover_tasks()
